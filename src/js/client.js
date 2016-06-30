4c3548630f8029f88b2f5f76472bb4adb39127ec
Daniel Levine <dlevine@marketwiise.com>
2016-06-30 17:46:23 -0500

    Persist the state to local storage

--- src/js/client.js
+++ src/js/client.js
@@ -5,7 +5,18 @@ import { createStore } from 'redux';
 import tryitApp from './reducers';
 import App from './components/App';
 
-let store = createStore(tryitApp);
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
+
+const persistedState = loadState();
+
+let store = createStore(tryitApp, persistedState);
+
+store.subscribe(throttle(() => {
+  saveState({
+    items: store.getState().items
+  });
+}, 1000));
 
 render(
   <Provider store={store}>
